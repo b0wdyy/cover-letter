6695af8e9abf8633745bf4df065dd7b3471a582d
Bodhi Vandael <bowdy@Bodhis-MacBook-Pro.local>
2024-02-13 23:12:57 +0100

    chore: no idea what I did, but updated eslint and such

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				oswald: ['Oswald', 'sans-serif'],
+				Montserrat: ['Montserrat', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/forms')],
+};
